843e55a7d3c8346c0788fecab42ebe19b9c22740
Yunfeng Wang <vra@users.noreply.github.com>
2016-10-06 22:21:15 +0800

    Create SingleNumber3.cpp

---
+++ SingleNumber3.cpp
@@ -0,0 +1,29 @@
+//Question: https://leetcode.com/problems/single-number-iii/
+//Explanation: https://discuss.leetcode.com/topic/21605/accepted-c-java-o-n-time-o-1-space-easy-solution-with-detail-explanations
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int>& nums) 
+    {
+        // Pass 1 : 
+        // Get the XOR of the two numbers we need to find
+        int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+        // Get its last set bit
+        diff &= -diff;
+
+        // Pass 2 :
+        vector<int> rets = {0, 0}; // this vector stores the two numbers we will return
+        for (int num : nums)
+        {
+            if ((num & diff) == 0) // the bit is not set
+            {
+                rets[0] ^= num;
+            }
+            else // the bit is set
+            {
+                rets[1] ^= num;
+            }
+        }
+        return rets;
+    }
+};
