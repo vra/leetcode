d324eba9350254ad23ba2595842468f3c032ff24
vra <wangyf11@mail.ustc.edu.cn>
2016-11-01 22:08:24 +0800

    add guessNumber

---
+++ cpp/guessNumber.cpp
@@ -0,0 +1,29 @@
+// Forward declaration of guess API.
+
+// @param num, your guess
+
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+
+int guess(int num);
+
+
+
+class Solution {
+
+public:
+
+    int guessNumber(int n) {
+        int low = 1 , high = n;
+        int mid;
+        while(low <=high) {
+            mid = (high-low)/2 +low;
+            if(guess(mid) == 0) return mid;
+            else if(guess(mid) == -1)
+                high = mid-1;
+            else
+                low = mid +1;
+        }
+
+        return -1;
+    }
+};
