cbbfe6a64bbf5e2dd0022959e89121f0c6a0f497
Yunfeng Wang <vra@users.noreply.github.com>
2016-11-18 06:20:38 -0600

    Create readBinaryWatch.cpp

---
+++ cpp/readBinaryWatch.cpp
@@ -0,0 +1,32 @@
+H h7540767 
+Reputation:  0
+ class Solution {
+private:
+    int onecnt(int num)
+    {
+        int ret = 0;
+        while(num)
+        {
+            num = num & (num - 1);
+            ret++;
+        }
+        return ret;
+    }
+public:
+    vector<string> readBinaryWatch(int num) {
+        vector<string> ret;
+        for (int h = 0; h < 12; h++)
+        {
+            for (int m = 0; m < 60; m++)
+            {
+                if (onecnt(h) + onecnt(m) == num)
+                {
+                    stringstream tmp;
+                    tmp << h << ":" << m / 10 << m % 10;
+                    ret.push_back(tmp.str());
+                }
+            }
+        }
+        return ret;
+    }
+};
