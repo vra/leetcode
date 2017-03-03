f41533d8c3b041f8fa00029667e4800e2626cdbd
Yunfeng Wang <vra@users.noreply.github.com>
2017-03-03 22:44:25 +0800

    Create hasCycle.cpp

---
+++ cpp/hasCycle.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+       ListNode* p = head;
+       ListNode* q = head;
+       while (p->next && q->next && q->next->next) {
+       	p = p->next;
+       	q = q->next->next;
+       	if (p == q->next) return true;
+       }
+
+       return false;
+    }
+};
